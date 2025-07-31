72a31ce4fe16e33a8afb9f198ad7f1f7252b4c6c
Juan Puerta <juan.pablo.puerta@correounivalle.edu.co>
2025-07-31 18:29:17 -0500

    database change

--- database_mongo.py
+++ database_mongo.py
@@ -9,7 +9,7 @@ MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB = os.getenv("MONGO_DB")
 
 try:
-    client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+    client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI, tls=True)
     database = client[MONGO_DB]
     print("Conectado a MongoDB correctamente")
 except Exception as e:
