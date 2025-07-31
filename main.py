3e030aa34b16d57e9e66920ccfbcf1b024def637
Juan Puerta <juan.pablo.puerta@correounivalle.edu.co>
2025-07-31 17:46:07 -0500

    cors for test/2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://3-d-organic-health.vercel.app"
+        "https://3-d-organic-health.vercel.app",
         "https://3-d-organic-health-git-jppg-ynhuks-projects.vercel.app"
     ],
     allow_credentials=True,
